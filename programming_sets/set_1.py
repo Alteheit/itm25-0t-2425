271a1dd26ec1aeaa3fab5582b5f7af8973a86b74
Joe Ilagan <joeilagan@Joes-MacBook-Pro-2.local>
2024-06-06 13:56:13 +0800

    removed bmi

--- programming_sets/set_1.py
+++ programming_sets/set_1.py
@@ -94,39 +94,3 @@ def interest(principal, rate, periods):
     # Replace `pass` with your code.
     # Stay within the function. Only use the parameters as input. The function should return your answer.
     pass
-
-def body_mass_index(weight, height):
-    '''Body Mass Index.
-    5 points.
-
-    This function calculates the body mass index (BMI) of a person
-        given their weight and height.
-
-    The formula for BMI is: kg / (m ^ 2)
-        (i.e., kilograms over meters squared)
-
-    Unfortunately, the users of this function use the imperial system.
-        You will need to first convert their arguments to the metric system.
-
-    Parameters
-    ----------
-    weight: float
-        the weight of the person, in pounds
-    height: list
-        the height of the person, expressed as a list of two integers.
-        the first integer is the foot component of their height.
-        the second integer is the inches component of their height.
-        for example, 5'10" would be passed as [5, 10].
-
-    We have not yet discussed lists, but use the skills you developed
-        in the command line exercise. How would you learn how to work with
-        lists?
-
-    Returns
-    -------
-    float
-        the BMI of the person.
-    '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
